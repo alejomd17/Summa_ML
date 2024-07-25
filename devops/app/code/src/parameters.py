90f89dd66405d1c9cf8186f185f6fa7647e277c7
Alejandro <jorge.moscoso@summa-sci.com>
2024-07-25 00:01:25 -0500

    Creation of the dockerfile

---
+++ devops/app/code/src/parameters.py
@@ -0,0 +1,30 @@
+import os
+
+class Parameters(object):
+    # General
+    steps                  = 4
+    col_pronos             = 'Demand'
+    col_y                  = 'year_month'
+    scales                 = [
+                            col_pronos+'_og',
+                            col_pronos+'_scal01',
+                            col_pronos+'_scal11',
+                            col_pronos+'_scallg'
+                            ]
+    
+    # Rooths for Models in Jupyter
+    root_path              = os.path.abspath(os.path.join('../'+os.path.dirname('__file__')))
+    input_path             = os.path.join(root_path, 'data', 'input')
+    output_path            = os.path.join(root_path, 'data', 'output')
+    results_path           = os.path.join(root_path, 'data', 'output', 'results')
+    models_path            = os.path.join(root_path, 'data', 'output', 'models')
+    plots_path             = os.path.join(root_path, 'data', 'output', 'plots')
+    
+    # Rooths for API
+    root_path_api          = os.path.abspath(os.path.join(os.path.dirname('__file__')))
+    results_path_api       = os.path.join(root_path_api, 'data', 'output', 'results')
+    
+    
+    
+
+    
